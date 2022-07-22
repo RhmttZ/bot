5258089fce9a9a4aa24ae6272ea504b93145fae6
RhmttZ <109515954+RhmttZ@users.noreply.github.com>
2022-07-22 18:42:49 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6287898307350'],
-  ['6287898307350'],
-  ['6287898307350', 'Owner', true]
+  ['6285254337661'],
+  ['6285254337661'],
+  ['6285254337661', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@penta_bot'
-  var sticker_author = 'penta'
+  var sticker_name = '@cyo_botz'
+  var sticker_author = 'RhmttZ'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Gempy'
+global.wm = '© RhmttZ'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
@@ -66,9 +66,9 @@ global.rpg = {
   emoticon(string) {
     string = string.toLowerCase()
     let emot = {
-      exp: '✉️',
+      exp: '✨',
       money: '💵',
-      potion: '🥤',
+      potion: '🧪',
       diamond: '💎',
       common: '📦',
       uncommon: '🎁',
